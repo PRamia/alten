0b7f1f4ec4d8e87748a49bd17f9fb5a3d93a869a
pramia <pedroramia@hotmail.com>
2023-10-04 18:33:14 +0200

    BDD test steps definition and POM files

--- automatizacion/cypress/e2e/wikipedia.cy.js
+++ automatizacion/cypress/e2e/wikipedia.cy.js
@@ -1,5 +1,21 @@
+import googleSearch from '../selectors/googleSearch.sel'
+import wikipediaPage from '../selectors/wikipediaPage.sel'
+
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
+  before(() => {
+    cy.visit('/?hl=es');
+    cy.rejectCookies();
+  })
+  it('should search on googgle.com for \'automatización\' ', () => {
+    cy.get(googleSearch.inputSearch).type('automatización{enter}');
+  })
+  it('should search for the Wikipedia link', () =>{
+
+  })
+  it('should check in which year the first automatic process was made', () =>{
+
+  })
+  it('should make a screenshot of the wikipedia site', () =>{
+
   })
 })
---
+++ automatizacion/cypress/selectors/googleSearch.sel.js
@@ -0,0 +1,4 @@
+module.exports = {
+    inputSearch: 'textarea[type="search"]',
+    searchButton: 'input[aria-label="Buscar con Google"]'
+}
